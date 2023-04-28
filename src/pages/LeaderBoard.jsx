02663435fced48639febc4e3fcc89ef8f8d257e5
Rahil <rahil.eliyev@gmail.com>
2023-04-28 13:21:43 +0400

    leaderboard page

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,8 +1,39 @@
+import { ReactComponent as Leaderboard } from "@/assets/images/leaderBoard.svg";
+import Partnership from "@/assets/images/partner-ship.svg";
+import { ReactComponent as ArrowRight } from "@/assets/images/arrow-right.svg";
+import LeaderBoardImage from "@/assets/images/Globe.png";
 
 const LeaderBoard = () => {
   return (
-    <div>LeaderBoard</div>
-  )
-}
+    <section className="homepage">
+      <div className="homepage-container container">
+        <div className="left">
+          <h6>
+            HPAY <span>Leaderboard</span>
+          </h6>
+          <ul>
+            <li>
+              <Leaderboard /> <span>Leaderboard</span> <ArrowRight className="arrow" />
+            </li>
+            <li className="active">
+              <img src={Partnership} alt="logo" />
+              <span>Partnerships</span> <ArrowRight className="arrow" />
+            </li>
+          </ul>
+        </div>
+        <div className="center">
+          <h3>Security Center</h3>
+          <p>
+            One of HedgePay’s three core tenants is security. Use our database to find projects with known leaders and
+            audited contracts.
+          </p>
+        </div>
+        <div className="right no-css">
+          <img src={LeaderBoardImage} alt="World" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
